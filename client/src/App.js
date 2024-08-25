b121a01daa2dcb66514e485bb1a9d3aca67a57a4
ChristianMcNachtan <christian.mcnachtan@gmail.com>
2024-08-25 01:05:43 -0400

    removed unused variables

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,14 @@ import TodoList from './components/toDoList';
 import './App.css'; // Import general styles here
 
 function App() {
-    const [theme, setTheme] = useState('light');
+    const [theme] = useState('light');
 
     useEffect(() => {
         // Apply the theme to the body element
         document.body.className = theme;
     }, [theme]);
 
-    const toggleTheme = () => {
-        setTheme(theme === 'light' ? 'dark' : 'light');
-    };
+  
 
     return (
         <div className={`App ${theme}`}>
