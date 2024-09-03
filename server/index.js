138bef86df8782065f1ad0518b5c71c2ed27e5c9
ChristianMcNachtan <christian.mcnachtan@gmail.com>
2024-09-03 19:47:37 -0400

    edited index.js

--- server/index.js
+++ server/index.js
@@ -11,10 +11,11 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: "mcnacdb.mysql.database.azure.com",
+  user: "McNacAdmin",
+  password: "DBPassword1!",
+  database: "mcnacdb",
+  port: 3306,
   ssl: {
     rejectUnauthorized: true
   }
